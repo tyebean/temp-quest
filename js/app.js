a960f41ceea5c2f5aaa2ac6ec9a048bf81f73b36
tyebean <flower.webster@gmail.com>
2022-02-14 11:56:12 -0500

    add hideTreasure

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+  //if there is a movement click, reduce the amount of searches left
+
+
 /*---------------------------- Constants ------------------------------------*/
 const moveArray = [
-  [2, 5],
-  [3, 8],
-  [9, 14],
-  [15, 12],
+  [2, 5], //1
+  [3, 8], //4
+  [9, 14], //13
+  [15,12], //16
   [8, 11, 16],
-  [3, 10, 15],
+  [3,10, 15],
   [14, 11, 16],
   [4, 7, 12],
   [5, 10, 3],
@@ -17,9 +20,19 @@ const moveArray = [
   [6, 9, 11, 14],
   [7, 10, 12, 15]
 ]
+
+// for (let i = 0; i < moveArray.length; i++){
+//   const a = moveArray[i][0]
+//   const b = moveArray[i][1]
+//   const c = moveArray[i][2]
+//   const d = moveArray[i][3]
+//   console.log(a, b, c, d)
+//   console.log(moveArray[a])
+// }
+
 /*---------------------------- Variables (state) ----------------------------*/
 
-let boardArray, player, treasure, win, lose
+let player, treasure, win, lose
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -29,7 +42,7 @@ const appleMoji = document.querySelector('#apple-emoji')
 const message = document.querySelector('#instruction-message')
 const btnSearch = document.querySelector('.btn-search')
 const btnReset = document.querySelector('.btn-reset') 
-const badgeNum = document.querySelector('.badge')
+const badgeNum = document.querySelector('.badge') 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -38,16 +51,17 @@ btnSearch.addEventListener('click', search)
 btnReset.addEventListener('click', reset)
 
 /*-------------------------------- Functions --------------------------------*/
+
 init()
 function init(){
+  console.log("init runs");
   player = 
   [null, null, null, null,
   null, null, null, null,
   null, null, null, null,
   null, null, null, null]
-  win = null 
+  win = null
   lose = null
-  console.log("init runs");
   message.textContent = "Click an Area to Search the Forest" 
   render()
   hideTreasure()
@@ -58,54 +72,60 @@ function render(){
   console.log('render runs')
 }
 
-//handleclick
-//if there's a click, place a snake there
-//style it approprietly
+// todo when a user has clicked, place the index of the position into the correct place in the player array. possibly using splice? 
 
+// todo style it approprietly/centered in the div
 function handleMovement(evt){
-  console.log("handle click/movement runs", evt.target)
-  const tileIdx = evt.target.id
-  // tiles[tileIdx].textContent = "🐍"
+  const idx = evt.target.id.replace("tile-", "")
+  tiles[idx].innerHTML = "hey"
+  // player.push(idx)
+  console.log(player)
+  console.log("clicking works")
   validateMove()
   removeSearch()
-  }
+}
 
 //validMove()
-//if there is no snake
+//if there is no value inside of player array 
 //move anywhere
 //if the snake is at [index]
 //can only move certain spaces
 
 function validateMove() {
   console.log("validating moves runs")
+  // const idx = evt.target.id.replace("tile-", "")
+  for (let i = 0; i < moveArray.length; i++){
+  if (player[i] !== null){
+    console.log("don't move anymore")
+    }
+  }
 }
 
 // ? search
-// probably to be broken down into even more functions UGH lol
-// flip shit
+// probably to be broken down into even more functions
 // if something got found, end game, display msg
 // if nothing found, display msg 
 function search(){
+  if (player[idx] === apple[idx]){
+    return
+  } else if (player[idx] !== apple[idx]) {
+    //nothing found msg
+  }
   console.log("search btn works")
 }
 
-
-
-
 function removeSearch(){
   //if there is a movement click, reduce the amount of searches left 
+    badgeNum.textContent = "4"
+    console.log(badgeNum)
   console.log("taking away your searches!!")
 }
 
-
 function hideTreasure(){
-  //if there is a movement click, reduce the amount of searches left 
-  console.log("hiding trasure")
+  const random = Math.floor(Math.random() * 15 - 0) + 0;
+  tiles[random].textContent = "🍎"
+  console.log("hiding trasure", random)
 }
-
-
-
-
 
 function reset(){
   console.log("reset btn works")
